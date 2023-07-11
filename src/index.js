abdd4d96393c382e2606c122b349498e5f5d972e
Lakshya Singh <lakshyaasin@gmail.com>
2023-07-12 00:55:40 +0530

    complete-frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import UserName from './UserName';
+import Footer from "./Footer";
+import SocketFront from './Socketfront';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<UserName />} />
+        <Route path='/game/' element={<SocketFront />} />
+        <Route />
+      </Routes>
+    </Router>
+    <Footer />
+  </div>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
